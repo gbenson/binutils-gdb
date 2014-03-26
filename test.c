140f0021253b03c774cbe78bc0d29e9852757b67
Gary Benson <gbenson@redhat.com>
2014-03-26 11:07:46 +0000

    New testcase from https://sourceware.org/bugzilla/show_bug.cgi?id=14963#c18

--- test.c
+++ test.c
@@ -2,13 +2,8 @@
 #include <stdlib.h>
 #include <demangle.h>
 
-const char *mangled = "_ZSt7forwardIRN1x14refobjiteratorINS0_3refINS0_" \
-  "4mime30multipart_section_processorObjIZ15get_body_parserIZZN14mime" \
-  "_processor21make_section_iteratorERKNS2_INS3_10sectionObjENS0_10pt" \
-  "rrefBaseEEEbENKUlvE_clEvEUlSB_bE_ZZNS6_21make_section_iteratorESB_" \
-  "bENKSC_clEvEUlSB_E0_ENS1_INS2_INS0_20outputrefiteratorObjIiEES8_EE" \
-  "EERKSsSB_OT_OT0_EUlmE_NS3_32make_multipart_default_discarderISP_EE" \
-  "EES8_EEEEEOT_RNSt16remove_referenceISW_E4typeE";
+const char *mangled = "_ZNSt9_Any_data9_M_accessIPZN13ThreadManager10fu" \
+  "tureTaskISt5_BindIFSt7_Mem_fnIM6RunnerFvvEEPS5_EEEEvOT_EUlvE_EERSC_v";
 
 int
 main (int argc, char *argv[])
