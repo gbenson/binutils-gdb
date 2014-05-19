1c041b7ef75ca9c247f63289abd84e0c421bd5d4
Gary Benson <gbenson@redhat.com>
2014-05-19 11:22:07 +0100

    Implement the flag

--- gdb/cp-support.c
+++ gdb/cp-support.c
@@ -1551,46 +1551,45 @@ gdb_demangle (const char *name, int options)
 #if defined (HAVE_SIGACTION) && defined (SA_RESTART)
   struct sigaction sa, old_sa;
 
-  sa.sa_handler = gdb_demangle_signal_handler;
-  sigemptyset (&sa.sa_mask);
-  sa.sa_flags = 0;
-  sigaction (SIGSEGV, &sa, &old_sa);
+  if (catch_demangler_crashes)
+    {
+      sa.sa_handler = gdb_demangle_signal_handler;
+      sigemptyset (&sa.sa_mask);
+      sa.sa_flags = 0;
+      sigaction (SIGSEGV, &sa, &old_sa);
+    }
 #else
   void (*ofunc) ();
 
-  ofunc = (void (*)()) signal (SIGSEGV, gdb_demangle_signal_handler);
+  if (catch_demangler_crashes)
+    ofunc = (void (*)()) signal (SIGSEGV, gdb_demangle_signal_handler);
 #endif
 
-  crash_signal = SIGSETJMP (gdb_demangle_jmp_buf);
+  if (catch_demangler_crashes)
+    crash_signal = SIGSETJMP (gdb_demangle_jmp_buf);
 #endif
 
   if (crash_signal == 0)
     result = bfd_demangle (NULL, name, options);
 
 #ifdef SIGSEGV
-#if defined (HAVE_SIGACTION) && defined (SA_RESTART)
-  sigaction (SIGSEGV, &old_sa, NULL);
-#else
-  signal (SIGSEGV, ofunc);
-#endif
-#endif
-
-  if (crash_signal != 0)
+  if (catch_demangler_crashes)
     {
-      static int error_reported = 0;
+#if defined (HAVE_SIGACTION) && defined (SA_RESTART)
+      sigaction (SIGSEGV, &old_sa, NULL);
+#else
+      signal (SIGSEGV, ofunc);
+#endif
 
-      if (!error_reported)
+      if (crash_signal != 0)
 	{
-	  internal_warning (__FILE__, __LINE__,
-			    _("unable to demangle '%s' "
-			      "(demangler failed with signal %d)"),
-			    name, crash_signal);
-
-	  error_reported = 1;
+	  internal_error (__FILE__, __LINE__,
+			  _("unable to demangle '%s' "
+			    "(demangler failed with signal %d)"),
+			  name, crash_signal);
 	}
-
-      result = NULL;
     }
+#endif
 
   return result;
 }
