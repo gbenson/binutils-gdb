b5ed46b440cbf965c8b16a9676b023f032a238ca
Gary Benson <gbenson@redhat.com>
2015-08-14 15:01:28 +0100

    Generate a correctly formatted note

--- test.c
+++ test.c
@@ -1,24 +1,28 @@
 /* gcc -shared -o libtest.so test.c */
 /* eu-readelf -n libtest.so */
 
+/* NB objcopy --add-section can add to pre-built files.*/
+/* NB objcopy is unconditionally usable at glibc build time */
+
 #include <sys/sdt.h>
+
+#define NT_GNU_INFINITY 5
+
+/* NB Add the above to glibc/src/elf/elf.h */
 
 static void
 dummyfunc ()
 {
+  /* .pushsection, .incbin, .popsection */
   __asm__ __volatile__ (
     _SDT_ASM_3 (.pushsection .note.infinity, "a", "note")
-    _SDT_ASM_1 (.byte 72)
-    _SDT_ASM_1 (.byte 101)
-    _SDT_ASM_1 (.byte 108)
-    _SDT_ASM_1 (.byte 108)
-    _SDT_ASM_1 (.byte 111)
-    _SDT_ASM_1 (.byte 32)
-    _SDT_ASM_1 (.byte 119)
-    _SDT_ASM_1 (.byte 111)
-    _SDT_ASM_1 (.byte 114)
-    _SDT_ASM_1 (.byte 108)
-    _SDT_ASM_1 (.byte 100)
+    _SDT_ASM_1 (.4byte 4)  /* Name size */
+    _SDT_ASM_1 (.4byte 13) /* Desc size */
+    _SDT_ASM_1 (.4byte NT_GNU_INFINITY)
+    _SDT_ASM_1 (.string "GNU")
+    _SDT_ASM_1 (.align 4)
+    _SDT_ASM_1 (.string "Hello world!")
+    _SDT_ASM_1 (.align 4)
     _SDT_ASM_1 (.popsection));
 }
 
