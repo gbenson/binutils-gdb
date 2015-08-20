0c8b7585168a34e62ec725ec13899265960782c6
Gary Benson <gbenson@redhat.com>
2015-08-20 16:52:42 +0100

    Oh, look, .4byte works now???

--- test1.c
+++ test1.c
@@ -11,9 +11,9 @@ main (int argc, char *argv[])
 {
 __asm__ __volatile__ (
   ".pushsection .note.infinity, \"a\", \"note\"\n"
-  ".byte 4, 0, 0, 0\n"  /* Name size */
-  ".byte 15, 0, 0, 0\n" /* Desc size INCLUDING 2 BYTE VERSION */
-  ".byte 23, 0, 0, 0\n" /* Note type */
+  ".4byte 4\n"  /* Name size */
+  ".4byte 15\n" /* Desc size INCLUDING 2 BYTE VERSION */
+  ".4byte 23\n" /* Note type */
   ".string \"GNU\"\n"
   ".align 4\n"
   ".byte 1\n"  /* Major version */
