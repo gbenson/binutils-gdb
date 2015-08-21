5af24c4b7664b32870ee5d33c7f56341534c25c3
Gary Benson <gbenson@redhat.com>
2015-08-21 11:00:32 +0100

    Bake in version 1 note format

--- test2.c
+++ test2.c
@@ -3,16 +3,25 @@ dummy(void)
 {
 __asm__ __volatile__ (
   "	.pushsection .note.infinity, \"a\", \"note\"\n"
+  "     .balign 4\n" /* should be unnecessary, but... */
   "     .4byte 102f-101f\n"  /* namesz */
   "     .4byte 104f-103f\n"  /* descsz */
   "     .4byte 23\n"         /* NT_GNU_INFINITY */
   "101: .string \"GNU\"\n"
   "102: .balign 4\n"
-  "103: .byte 1\n"               /* version */
-  "     .byte 0\n"               /* reserved MUST BE ZERO */
-  "     .byte 0\n"               /* reserved MUST BE ZERO */
-  "     .byte 0\n"               /* reserved MUST BE ZERO */
-  "     .string \"td_ta_map_lwp2thr\"\n" /* name + '\0' */
-  "104: .balign 4\n"
+  "103: .byte 0, 1\n"                   /* version */
+  "     .byte 0, 0\n"                   /* reserved1 (must be zero) */
+  "     .byte 0, 0\n"                   /* num_args */
+  "     .byte 0, 0\n"                   /* num_constants */
+  "     .byte 0, 0\n"                   /* max_locals (must be >= num_args) */
+  "     .byte 0, 0\n"                   /* max_stack */
+  "     .byte 0, 0, 0, 0\n"             /* reserved2 (must be zero) */
+  "     .string \"libpthread\"\n"       /* provider + '\0' */
+  "     .balign 4\n"
+  "     .string \"ta_map_lwp2thr\"\n"   /* name + '\0' */
+  "     .balign 4\n"
+  "     .byte 14, 23, 12, 118\n"        /* instructions */
+  "     .byte 12, 242, 13, 42\n"
+  "104: .balign 4\n" /* should be unnecessary (instructions are 4 bytes) */
   "     .popsection\n");
 }
